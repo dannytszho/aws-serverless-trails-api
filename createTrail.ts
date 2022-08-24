ab87940372b10bd257cf18e8b2f15ce40545cddf
dannytszho <dannytszho@gmail.com>
2022-08-23 21:43:45 -0500

    Add serverless offline

--- createTrail.ts
+++ createTrail.ts
@@ -1,28 +1,37 @@
 "use strict";
 import * as AWS from "aws-sdk";
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 import { PutItemInput } from "aws-sdk/clients/dynamodb";
 
-module.exports.createTrail = async (event) => {
-  const body = JSON.parse(event.body);
+module.exports.createTrail = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const timestamp = new Date().getTime().toString();
+  const trail = JSON.parse(event.body!);
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
   const putParams: PutItemInput = {
     TableName: process.env.DYNAMODB_TRAILS_TABLE!,
     Item: {
-      // primary_key: body.name,
-      primary_key: body.name,
-      email: body.email,
-      // length: body.length,
-      // elevation: body.elevation,
-      // duration: body.duration,
-      // difficulty: body.difficulty,
-      // rating: body.rating,
-      // url: body.url,
-      // imageUrl: body.imageUrl,
+      primary_key: trail.name,
+      length: trail.length,
+      elevation: trail.elevation,
+      duration: trail.duration,
+      difficulty: trail.difficulty,
+      rating: trail.rating,
+      url: trail.url,
+      imageUrl: trail.imageUrl,
+      // createdAt: trail.timestamp,
+      // updatedAt: timestamp
     },
   };
   await dynamoDb.put(putParams).promise();
 
   return {
     statusCode: 201,
+    body: JSON.stringify(trail),
   };
 };
