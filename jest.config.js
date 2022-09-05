a9e6754032eb927ab945d0e8fb5d4310e5e23f0a
dannytszho <dannytszho@gmail.com>
2022-09-05 18:59:56 -0500

    Add coverageThreshold in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,js}", "!**/node_modules/**"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      function: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
